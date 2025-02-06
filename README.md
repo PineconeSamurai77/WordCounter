05ba2a2bec50917cd77dc48d2be88340be9b8874
PineconeSamurai77 <eycrescentau62nexus81@gmail.com>
2025-02-06 20:12:16 +0000

    WordCounter

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WordCounter
 A program to count the number of words in a given string
+rock613
---
+++ WordCounter.py
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "strings"
+)
+
+func main() {
+    fmt.Println("Enter a string:")
+    reader := bufio.NewReaderSize(strings.NewReader("This is a Go example"), 1024)
+    input, _ := reader.ReadString( word Counter ....
